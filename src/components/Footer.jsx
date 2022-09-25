f6832a85bf8d8030c31233727d91bb7a69de118a
nidhikumari <kumari9936nidhi@gmail.com>
2022-09-25 15:27:37 +0530

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <>
-            <div className="footer h-20 bg-red-600 text-center">
+            <div className="footer h-20 bg-slate-700 text-center">
                 <p className="text-slate-50 font-semibold text-xl p-5">Made with ♥️ by Nidhi</p>
             </div>
         </>
