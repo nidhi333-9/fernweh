967105198c5ce25a66336d58a933d85e097b347b
nidhikumari <kumari9936nidhi@gmail.com>
2023-01-19 20:22:24 +0530

    some changes in data

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,103 +1,122 @@
 export default [
-    {
-        id: 1,
-        title: "Mount Fuji",
-        location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
-    },
-    {
-        id: 2,
-        title: "Great Wall of China",
-        location: "China",
-        googleMapsUrl: "https://goo.gl/maps/7NDtM4LYrHxSkdHM8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "The Great Wall of China became a UNESCO World Heritage Site in December 1987. The wall is the longest man made structure in the world, with a total length of about 13170.7 mi or 21196.18 km.",
-        imageUrl: "https://www.edreams.com/blog/wp-content/uploads/sites/3/2013/07/shutterstock_1398074603.jpg.webp"
-    },
-    {
-        id: 3,
-        title: "Preacher’s Pulpit Preikestolen",
-        location: "Norway",
-        googleMapsUrl: "https://goo.gl/maps/bYfyYoMdsWqjQ6oB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Preikestolen or Prekestolen (English: 'The Pulpit Rock', 'Pulpit', or 'Preacher's Chair') is a tourist attraction in the municipality of Strand in Rogaland county, Norway. Preikestolen is a steep cliff which rises 604 metres (1,982 ft) above Lysefjorden.",
-        imageUrl: "https://www.edreams.com/blog/wp-content/uploads/sites/3/2013/07/shutterstock_1221929191.jpg.webp"
-    },
-    {
-        id: 4,
-        title: "Machu Picchu",
-        location: "Peru",
-        googleMapsUrl: "https://goo.gl/maps/cp3SXvaBvnGqW41bA",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "More than 7,000 feet above sea level in the Andes Mountains, Machu Picchu is the most visited tourist destination in Peru. A symbol of the Incan Empire and built around 1450AD, Machu Picchu was designated a UNESCO World Heritage Site in 1983.",
-        imageUrl: "https://www.edreams.com/blog/wp-content/uploads/sites/3/2013/07/shutterstock_168497345.jpg.webp"
-    },
-    {
-        id: 5,
-        title: "The temples at Petra",
-        location: "Jordan",
-        googleMapsUrl: "https://goo.gl/maps/K7gSiKnYbnFiy91e9",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "The so-called Great Temple at Petra is a grand monumental complex that lies south of the Colonnaded Street at Petra. This wonder of the world is the most valuable treasure in Jordan, and also its main tourist attraction in the country.",
-        imageUrl: "https://www.edreams.com/blog/wp-content/uploads/sites/3/2013/07/shutterstock_1406860232.jpg.webp"
-    },
-    {
-        id: 6,
-        title: "Quilotoa Lake",
-        location: "Ecuador",
-        googleMapsUrl: "https://goo.gl/maps/oRR8Cbvnm64nEt658",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "A few hours south of Quito, Quilotoa Lake is a striking 3km-wide volcanic crater lake which was formed over 800 years ago when a huge eruption caused the volcano to collapse.",
-        imageUrl: "https://www.edreams.com/blog/wp-content/uploads/sites/3/2013/07/shutterstock_651962659.jpg.webp"
-    },
-    {
-        id: 7,
-        title: "The Meenakshi Temple",
-        location: "India",
-        googleMapsUrl: "https://goo.gl/maps/efvhYi7xg8FEi944A",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "It is dedicated to the goddess Meenakshi, a form of Parvati, and her consort, Sundareshwarar, a form of Shiva. The temple is at the center of the ancient temple city of Madurai mentioned in the Tamil Sangam literature, with the goddess temple mentioned in 6th-century-CE texts.",
-        imageUrl: "https://www.edreams.com/blog/wp-content/uploads/sites/3/2013/07/shutterstock_1013396287.jpg.webp"
-    },
-    {
-        id: 8,
-        title: "Plitvice Lakes National Park",
-        location: " Croatia",
-        googleMapsUrl: "https://goo.gl/maps/y6n48if7JACaiG8W9",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "The waters flowing over the limestone and chalk have, over thousands of years, deposited travertine barriers, creating natural dams which in turn have created a series of beautiful lakes, caves and waterfalls.",
-        imageUrl: "https://www.edreams.com/blog/wp-content/uploads/sites/3/2013/07/shutterstock_65988730.jpg.webp"
-    },
-    {
-        id: 9,
-        title: "Bora Bora",
-        location: "French Polynesia",
-        googleMapsUrl: "https://goo.gl/maps/Kw8HLVov3453NhwT9",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Bora Bora has a total land area of 30.55 km2 (12 sq mi). The main island, located about 230 kilometres (125 nautical miles) northwest of Papeete, is surrounded by a lagoon and a barrier reef.",
-        imageUrl: "https://www.edreams.com/blog/wp-content/uploads/sites/3/2013/07/shutterstock_768689953.jpg.webp"
-    },
-    {
-        id: 10,
-        title: "Salar de Uyuni",
-        location: "Bolivia",
-        googleMapsUrl: "https://goo.gl/maps/r6FnzJVQdVytZvX77",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Salar de Uyuni is the world's largest salt flat, or playa, at over 10,000 square kilometres (3,900 sq mi) in area. It is in the Daniel Campos Province in Potosí in southwest Bolivia, near the crest of the Andes at an elevation of 3,656 m (11,995 ft) above sea level.",
-        imageUrl: "https://www.edreams.com/blog/wp-content/uploads/sites/3/2013/07/shutterstock_81629539.jpg.webp"
-    },
-
-]
+  {
+    id: 1,
+    title: "Mount Fuji",
+    location: "Japan",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    imageUrl:
+      "https://th.bing.com/th/id/OIP.Sf2N_7pMwv4apVxJd1JjAwHaEo?w=290&h=181&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+  },
+  {
+    id: 2,
+    title: "Great Wall of China",
+    location: "China",
+    googleMapsUrl: "https://goo.gl/maps/7NDtM4LYrHxSkdHM8",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description:
+      "The Great Wall of China became a UNESCO World Heritage Site in December 1987. The wall is the longest man made structure in the world, with a total length of about 13170.7 mi or 21196.18 km.",
+    imageUrl:
+      "https://www.edreams.com/blog/wp-content/uploads/sites/3/2013/07/shutterstock_1398074603.jpg.webp",
+  },
+  {
+    id: 3,
+    title: "Preacher’s Pulpit Preikestolen",
+    location: "Norway",
+    googleMapsUrl: "https://goo.gl/maps/bYfyYoMdsWqjQ6oB8",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description:
+      "Preikestolen or Prekestolen (English: 'The Pulpit Rock', 'Pulpit', or 'Preacher's Chair') is a tourist attraction in the municipality of Strand in Rogaland county, Norway. Preikestolen is a steep cliff which rises 604 metres (1,982 ft) above Lysefjorden.",
+    imageUrl:
+      "https://www.edreams.com/blog/wp-content/uploads/sites/3/2013/07/shutterstock_1221929191.jpg.webp",
+  },
+  {
+    id: 4,
+    title: "Machu Picchu",
+    location: "Peru",
+    googleMapsUrl: "https://goo.gl/maps/cp3SXvaBvnGqW41bA",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description:
+      "More than 7,000 feet above sea level in the Andes Mountains, Machu Picchu is the most visited tourist destination in Peru. A symbol of the Incan Empire and built around 1450AD, Machu Picchu was designated a UNESCO World Heritage Site in 1983.",
+    imageUrl:
+      "https://www.edreams.com/blog/wp-content/uploads/sites/3/2013/07/shutterstock_168497345.jpg.webp",
+  },
+  {
+    id: 5,
+    title: "The temples at Petra",
+    location: "Jordan",
+    googleMapsUrl: "https://goo.gl/maps/K7gSiKnYbnFiy91e9",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description:
+      "The so-called Great Temple at Petra is a grand monumental complex that lies south of the Colonnaded Street at Petra. This wonder of the world is the most valuable treasure in Jordan, and also its main tourist attraction in the country.",
+    imageUrl:
+      "https://www.edreams.com/blog/wp-content/uploads/sites/3/2013/07/shutterstock_1406860232.jpg.webp",
+  },
+  {
+    id: 6,
+    title: "Quilotoa Lake",
+    location: "Ecuador",
+    googleMapsUrl: "https://goo.gl/maps/oRR8Cbvnm64nEt658",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description:
+      "A few hours south of Quito, Quilotoa Lake is a striking 3km-wide volcanic crater lake which was formed over 800 years ago when a huge eruption caused the volcano to collapse.",
+    imageUrl:
+      "https://www.edreams.com/blog/wp-content/uploads/sites/3/2013/07/shutterstock_651962659.jpg.webp",
+  },
+  {
+    id: 7,
+    title: "The Meenakshi Temple",
+    location: "India",
+    googleMapsUrl: "https://goo.gl/maps/efvhYi7xg8FEi944A",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description:
+      "It is dedicated to the goddess Meenakshi, a form of Parvati, and her consort, Sundareshwarar, a form of Shiva. The temple is at the center of the ancient temple city of Madurai mentioned in the Tamil Sangam literature, with the goddess temple mentioned in 6th-century-CE texts.",
+    imageUrl:
+      "https://www.edreams.com/blog/wp-content/uploads/sites/3/2013/07/shutterstock_1013396287.jpg.webp",
+  },
+  {
+    id: 8,
+    title: "Plitvice Lakes National Park",
+    location: " Croatia",
+    googleMapsUrl: "https://goo.gl/maps/y6n48if7JACaiG8W9",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description:
+      "The waters flowing over the limestone and chalk have, over thousands of years, deposited travertine barriers, creating natural dams which in turn have created a series of beautiful lakes, caves and waterfalls.",
+    imageUrl:
+      "https://www.edreams.com/blog/wp-content/uploads/sites/3/2013/07/shutterstock_65988730.jpg.webp",
+  },
+  {
+    id: 9,
+    title: "Bora Bora",
+    location: "French Polynesia",
+    googleMapsUrl: "https://goo.gl/maps/Kw8HLVov3453NhwT9",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description:
+      "Bora Bora has a total land area of 30.55 km2 (12 sq mi). The main island, located about 230 kilometres (125 nautical miles) northwest of Papeete, is surrounded by a lagoon and a barrier reef.",
+    imageUrl:
+      "https://www.edreams.com/blog/wp-content/uploads/sites/3/2013/07/shutterstock_768689953.jpg.webp",
+  },
+  {
+    id: 10,
+    title: "Salar de Uyuni",
+    location: "Bolivia",
+    googleMapsUrl: "https://goo.gl/maps/r6FnzJVQdVytZvX77",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description:
+      "Salar de Uyuni is the world's largest salt flat, or playa, at over 10,000 square kilometres (3,900 sq mi) in area. It is in the Daniel Campos Province in Potosí in southwest Bolivia, near the crest of the Andes at an elevation of 3,656 m (11,995 ft) above sea level.",
+    imageUrl:
+      "https://www.edreams.com/blog/wp-content/uploads/sites/3/2013/07/shutterstock_81629539.jpg.webp",
+  },
+];
